01aeebbcb7440960098baeab03c046eab1cf4d15
Dxnqa <deandreydomingo@gmail.com>
2025-11-09 18:45:00 -0900

    Appropriate ENV var created = BLOB_CONTAINER

--- database/Storage.py
+++ database/Storage.py
@@ -6,10 +6,11 @@ import os
 load_dotenv()
 
 ACCOUNT_URL = os.getenv("AZURE_ACCOUNT_URL")
+BLOB_CONTAINER = os.getenv("BLOB_CONTAINER")
 credential = DefaultAzureCredential()
 blob_service_client = BlobServiceClient(account_url=ACCOUNT_URL, credential=credential)
 
-container_client = blob_service_client.get_container_client("documentation")
+container_client = blob_service_client.get_container_client(BLOB_CONTAINER)
 
 for blob in container_client.list_blobs():
     print(blob.name)
